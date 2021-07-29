c330ceecd37b9c885e966355d36a1f5458e18cca
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-07-29 19:33:07 +0400

    test correction

--- src/js/__tests__/GameController.test.js
+++ src/js/__tests__/GameController.test.js
@@ -20,3 +20,69 @@ test('template literals', () => {
   gameCtrl.onCellEnter(8);
   expect(gamePlay.cells[8].title).toBe('');
 });
+
+test('onCellEnter Teammate', () => {
+  const gamePlay = new GamePlay();
+  const container = document.createElement('div');
+  container.outerHTML = '<div id="game-container"></div>';
+  gamePlay.bindToDOM(container);
+
+  const gameCtrl = new GameController(gamePlay, {});
+  gameCtrl.init();
+
+  gameCtrl.playerTeam.add(new PositionedCharacter(new Swordsman(5), 0));
+  gameCtrl.playerTeam.add(new PositionedCharacter(new Swordsman(1), 3));
+  gameCtrl.onCellClick(0);
+  gameCtrl.onCellEnter(3);
+  expect([...gamePlay.cells[0].classList]).toEqual(expect.arrayContaining(['selected-yellow']));
+  expect(gamePlay.boardEl.style.cursor).toBe('pointer');
+});
+
+test('onCellEnter Enemy', () => {
+  const gamePlay = new GamePlay();
+  const container = document.createElement('div');
+  container.outerHTML = '<div id="game-container"></div>';
+  gamePlay.bindToDOM(container);
+
+  const gameCtrl = new GameController(gamePlay, {});
+  gameCtrl.init();
+
+  gameCtrl.playerTeam.add(new PositionedCharacter(new Swordsman(5), 0));
+  gameCtrl.enemyTeam.add(new PositionedCharacter(new Daemon(1), 1));
+  gameCtrl.onCellClick(0);
+  gameCtrl.onCellEnter(1);
+  expect([...gamePlay.cells[1].classList]).toEqual(expect.arrayContaining(['selected-red']));
+  expect(gamePlay.boardEl.style.cursor).toBe('crosshair');
+});
+
+test('onCellEnter Step', () => {
+  const gamePlay = new GamePlay();
+  const container = document.createElement('div');
+  container.outerHTML = '<div id="game-container"></div>';
+  gamePlay.bindToDOM(container);
+
+  const gameCtrl = new GameController(gamePlay, {});
+  gameCtrl.init();
+
+  gameCtrl.playerTeam.add(new PositionedCharacter(new Swordsman(5), 0));
+  gameCtrl.onCellClick(0);
+  gameCtrl.onCellEnter(1);
+  expect([...gamePlay.cells[1].classList]).toEqual(expect.arrayContaining(['selected-green']));
+  expect(gamePlay.boardEl.style.cursor).toBe('pointer');
+});
+
+test('onCellEnter Not-allowed', () => {
+  const gamePlay = new GamePlay();
+  const container = document.createElement('div');
+  container.outerHTML = '<div id="game-container"></div>';
+  gamePlay.bindToDOM(container);
+
+  const gameCtrl = new GameController(gamePlay, {});
+  gameCtrl.init();
+
+  gameCtrl.playerTeam.add(new PositionedCharacter(new Swordsman(5), 0));
+  gameCtrl.onCellClick(0);
+  gameCtrl.onCellEnter(6);
+  expect([...gamePlay.cells[1].classList]).not.toEqual(expect.arrayContaining(['selected-green']));
+  expect(gamePlay.boardEl.style.cursor).toBe('not-allowed');
+});
