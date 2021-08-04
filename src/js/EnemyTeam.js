c35c087a67bd98cdec63eeeb67cfb12aefcc365d
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-08-04 19:01:20 +0400

    enemy team

--- src/js/EnemyTeam.js
+++ src/js/EnemyTeam.js
@@ -14,6 +14,7 @@ export default class EnemyTeam extends Team {
     if (this.attack(playerPositioned)) {
       return this.attack(playerPositioned);
     }
+    this.step(playerPositioned);
     return null;
   }
 
@@ -38,5 +39,71 @@ export default class EnemyTeam extends Team {
 
     const bestAttack = [].concat(...canAttack).sort((a, b) => a.coef - b.coef);
     return bestAttack[0];
+  }
+
+  step(playerPositioned) {
+    const boardSize = 8;
+    const distances = [];
+
+    this.positioned.forEach((member) => {
+      playerPositioned.forEach((character) => {
+        distances.push({
+          member,
+          targetIndex: character.position,
+          distance: EnemyTeam.calcSteps(member, character, boardSize),
+        });
+      });
+    });
+
+    distances.sort((a, b) => {
+      if (a.distance < b.distance) return -1;
+      if (a.distance > b.distance) return 1;
+      // if distance equal
+      if (a.member.character.attack > b.member.character.attack) return -1;
+      if (a.member.character.attack < b.member.character.attack) return 1;
+      return 0;
+    });
+
+    const bestMove = EnemyTeam.bestMove(distances[0].member, distances[0].targetIndex, boardSize);
+    for (let i = 0; i < bestMove.length; i += 1) {
+      if ([...playerPositioned, ...this.positioned]
+        .findIndex((character) => character.position === bestMove[i].stepIndex) < 0) {
+        distances[0].member.position = bestMove[i].stepIndex;
+        break;
+      }
+    }
+  }
+
+  static calcSteps(index, target, boardSize) {
+    const vertical = Math.abs(
+      Math.floor(index.position / boardSize) - Math.floor(target.position / boardSize),
+    );
+    const horizontal = Math.abs(
+      Math.floor(index.position % boardSize) - Math.floor(target.position % boardSize),
+    );
+    const vertSteps = Math.ceil(
+      (vertical - index.character.attackRadius) / index.character.stepRadius,
+    );
+    const horSteps = Math.ceil(
+      (horizontal - index.character.attackRadius) / index.character.stepRadius,
+    );
+    if (vertSteps < horSteps) {
+      return horSteps > 0 ? horSteps : 0;
+    }
+    return vertSteps > 0 ? vertSteps : 0;
+  }
+
+  static bestMove(index, target, boardSize) {
+    const bestStep = [];
+    index.stepCells.forEach((stepIndex) => {
+      const vertical = Math.abs(
+        Math.floor(stepIndex / boardSize) - Math.floor(target / boardSize),
+      );
+      const horizontal = Math.abs(
+        Math.floor(stepIndex % boardSize) - Math.floor(target % boardSize),
+      );
+      bestStep.push({ stepIndex, result: vertical + horizontal - index.character.attackRadius });
+    });
+    return bestStep.sort((a, b) => a.result - b.result);
   }
 }
